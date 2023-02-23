a4fd856b02e57b426bfe7a59b047743d85dfde8a
Asim.Sapkota <asim@3bird.nl>
2023-02-23 11:20:54 +0545

    project euler solution 1 and module refactor.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,3 @@
 pub mod helper;
-
-pub fn soln_1(num1: u32, num2: u32, limit: u32) -> u32 {
-    let sumto_num1 = (limit - 1) / num1;
-    let sumto_num2 = (limit - 1) / num2;
-
-    let lcm = helper::lcm(num1, num2);
-    let sumto_lcm = (limit - 1) / lcm;
-
-    num1 * helper::sum_of_num(sumto_num1) + num2 * helper::sum_of_num(sumto_num2) - lcm * helper::sum_of_num(sumto_lcm)
-}
-
+pub mod soln1;
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use project_euler::*;
 
 fn main() {
-    println!("solution 1 : {}", soln_1(3, 5, 1000));
+    println!("solution 1 : {}", soln1::soln_1(3, 5, 1000));
 }
